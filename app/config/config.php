26e4130deba070b71d38a5910734cd51cd15c066
astinurin <150427543+astinurin@users.noreply.github.com>
2023-12-08 13:43:14 +0700

    Update database name, logo, and icons. (generated by blackbox.ai )

--- app/config/config.php
+++ app/config/config.php
@@ -5,4 +5,4 @@ define('BASEURL', 'http://localhost/mumetbolo_peminjamanruang_2d/public');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'peminjaman_ruangan2');
+define('DB_NAME', 'peminjaman_ruangan');
