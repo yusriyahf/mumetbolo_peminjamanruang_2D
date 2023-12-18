c7fa051cdb0224616409fa2548d220db2cbcc03b
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2023-12-18 17:20:23 +0700

    memperbaiki fungsi count beranda mhs

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -82,7 +82,7 @@
 
         <!-- Total Dosen Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <a href="#" class="text-decoration-none">
+            <a href="<?= BASEURL; ?>/mahasiswa/peminjamanDiAcc" class="text-decoration-none">
                 <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -103,21 +103,24 @@
 
         <!-- Peminjaman Ditolak Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-danger shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Peminjaman ditolak</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $data['totalDitolak']; ?></div>
-                        </div>
-                        <div class="col-auto">
-                            <span class="text-gray-300">
-                                <i class="fa fa-window-close fa-2x" aria-hidden="true"></i>
+            <a href="<?= BASEURL; ?>/mahasiswa/peminjamanDiTolak" class="text-decoration-none">
+
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                    Peminjaman ditolak</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $data['totalDitolak']; ?></div>
+                            </div>
+                            <div class="col-auto">
+                                <span class="text-gray-300">
+                                    <i class="fa fa-window-close fa-2x" aria-hidden="true"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
         <!-- alur jalan -->
 
